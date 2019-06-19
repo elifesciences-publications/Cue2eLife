bcb385fd74450a3c7983a659a699cf24ddbaa98f
greenlabjhmi <greenlabjhmi@gmail.com>
2019-06-19 10:13:36 -0400

    Update yeastdensebuilder_main.py

--- yeastdensebuilder_main.py
+++ yeastdensebuilder_main.py
@@ -1,4 +1,3 @@
-__author__ = 'Colin Wu'
 # This file creates ribosome density data structures.
 import GFF, argparse, pysam
 from Bio import Seq
@@ -15,12 +14,10 @@ class densebuilder(object):
 		self.riboshiftdict= riboshiftdict
 		self.bamfileout= bamfileout
 		self.softclipped= softclipped
-
-	# For alignments using bowtie2 and output bam files 
-	def setdense(self):
-		outputdata1, outputdata2= {}, {} # +1 strand and -1 strand
 		
-		 # Create BioSeq files from the GFF file for modification of the letter annotations field.
+	def setdense(self):
+		outputdata1, outputdata2= {}, {}
+		
 		for sequence in self.GFFgen:
 			outputdata1[sequence.id]= [0 for x in range(len(sequence))]
 			outputdata2[sequence.id]= [0 for x in range(len(sequence))]
@@ -37,11 +34,9 @@ class densebuilder(object):
 		else:
 			print str(self.totreads)+ " total reads entered. Using this for normalization."
 		
-		#Now we normalize data.
 		self.norm_m(outputdata1,(self.totreads))
 		self.norm_m(outputdata2,(self.totreads))
 		
-		# Write to file as float or pickle them...
 		self.writecountsf(outputdata1, self.densitypathandfilestring+"_plus_")
 		self.writecountsf(outputdata2, self.densitypathandfilestring+"_minus_")
 
@@ -49,8 +44,6 @@ class densebuilder(object):
 			self.countstowig(outputdata1, self.wigpathandfile+"_plus")
 			self.countstowig(outputdata2, self.wigpathandfile+"_minus")
 
-
-		# Write output file of comments.
 		fc= open(self.densitypathandfilestring+"_output.txt","w")
 		fc.write("Density was built with parameters:\n")
 		fc.write("riboshiftdict="+str(self.riboshiftdict)+"\n")
@@ -59,7 +52,6 @@ class densebuilder(object):
 		fc.write(str(mapped[1])+" reads of interested length mapped to the genome and splice junctions."+ "\n")
 
 
-	###### Setdense for 5'end alignment.
 	def setdense_5or3assignment(self,outputdata1,outputdata2,bamfiles,riboshiftdict,assignment,bamfileout,softclipped):
 		mappedreads= 0
 		mappedreadslocal= 0
@@ -71,11 +63,11 @@ class densebuilder(object):
 			print "bam file is "+ bam
 			bamgen= pysam.AlignmentFile(bam, "rb")
 
-			for read in bamgen.fetch():	# Iterate through all reads
-				if read.flag== 4 or read.flag== 256 or read.flag== 272:	continue # A bowtie mismatch. These are the same as those previously saved in the files bowtie output as reads without matches and reads exceeding the -m limit. No need to write as fastq.
-				
+			for read in bamgen.fetch():
+				if read.flag== 4 or read.flag== 256 or read.flag== 272:	continue
+
 				chrom= read.reference_name
-				startp= int(read.pos)   # in bam files, first position is ZERO. STAR outputs soft clipped startpos
+				startp= int(read.pos) 
 
 				totaljunctlen= 0
 				quitsignal= 0
@@ -83,20 +75,18 @@ class densebuilder(object):
 
 				readlen= 0
 				for element in read.cigar:
-					if element[0]== 0:	# (M) alignemnt match
+					if element[0]== 0:
 						readlen+= element[1]
-					elif element[0]== 1: # (I) insertion to reference 
-						#readlen-= element[1] 
+					elif element[0]== 1:
 						quitsignal= 1
-					elif element[0]== 2: # (D) deletion from the reference
-						#readlen+= element[1]
+					elif element[0]== 2: 
 						quitsignal= 1
-					elif element[0]== 3: # (N) skipped region from the reference, --> junction(s) in mapped reads... should be okay for ~30nt 
+					elif element[0]== 3:
 						totaljunctlen+= element[1]
 						junctreads+= 1
-					elif element[0]== 4:	# (S) soft clipping 
-						if element[1] <= 2:	readlen-= element[1] 	# For nontemplated addition...
-						if element[1] >2 and element[1]<= int(softclipped):	readlen+= element[1]  #pass     # Don't count clipped As #
+					elif element[0]== 4:
+						if element[1] <= 2:	readlen-= element[1] 
+						if element[1] >2 and element[1]<= int(softclipped):	readlen+= element[1] 
 						if element[1]> int(softclipped):	quitsignal= 1
 					else:	quitsignal= 1
 
@@ -113,11 +103,11 @@ class densebuilder(object):
 				if read.flag== 0 or read.flag== 16:	mappedreadslocal+= 1
  
 				if self.assignment== '5':
-					if (read.flag== 0):             # Align to the forward strand
+					if (read.flag== 0): 
 						ribojunctlen= self.junctlen_for_riboshift(read.cigar, riboshift)  
 						outputdata1[chrom][startp+ ribojunctlen+ riboshift]+= 1 
 
-					if (read.flag== 16):            # Align to the reverse strand
+					if (read.flag== 16):    
 						ribojunctlen= self.junctlen_for_riboshift(read.cigar[::-1], riboshift) 
 						outputdata2[chrom][startp+ totaljunctlen+ (readlen- 1)- ribojunctlen- riboshift]+= 1 
 
@@ -138,25 +128,22 @@ class densebuilder(object):
 		return [mappedreads,mappedreadslocal+junctreads]
 
 
-	# Function to determine the combined junction length for riboshift. 
 	def junctlen_for_riboshift(self,readcigar,riboshift):
 		junctnum= 0
 		mappedexonlen= 0
 		insert= 0
 		for x in range(len(readcigar)):
-			if readcigar[x][0]== 0:	# Match
+			if readcigar[x][0]== 0:
 				mappedexonlen+= readcigar[x][1]
 			if mappedexonlen-1 < riboshift:
 				junctnum+= 1
 
 		for y in range(junctnum):
-			if readcigar[y][0]== 3:	# Insert
+			if readcigar[y][0]== 3:
 				insert+= readcigar[y][1]
 		return insert
 
 
-	# Divide every read value by number of mapped reads. Values then in rpm.
-	# No need to return readcounts since it is a mutable type being a list.
 	def norm_m(self,readcounts,reads):
 	    for chrom in readcounts.keys():
 	        for position in range(len(readcounts[chrom])):
@@ -164,8 +151,7 @@ class densebuilder(object):
 	            readcounts[chrom][position]*= 1E6     
 
 
-	# Function to write counts files to disk - Use for float.
-	def writecountsf(self,resultlist,filestring):  #Resultlist it is the list of counts, filestring is the file prefix for each chr to be written.
+	def writecountsf(self,resultlist,filestring): 
 	    import struct
 	    f2= open(filestring+ "keys","w")
 	    for chrom in resultlist.keys():
@@ -192,10 +178,9 @@ if __name__== '__main__':
 	args = parser.parse_args()
 
 	import ast
-	riboshiftdict= ast.literal_eval(args.riboshiftdict) #convert string into dictionary
+	riboshiftdict= ast.literal_eval(args.riboshiftdict) 
 	GFFgen= GFF.parse(args.GFFfile)
 	bamgen0= pysam.AlignmentFile(args.bamfile1, "rb")
 	bamfileout= pysam.AlignmentFile(args.bamfileoutput, "wb", template= bamgen0)
 	rfpdense= densebuilder(GFFgen,args.bamfile1,args.bamfile2,args.densitypathandfilestring,args.wigpathandfile,args.totreads,args.assignment,riboshiftdict,bamfileout,args.softclipped)
 	rfpdense.setdense()
-	    
